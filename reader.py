deaf58426dbf566b782f8906ad0a61f989d0ca4c
John <johnwilles@gmail.com>
2017-05-28 23:40:36 -0400

    Add GPIO logic to reader

--- reader.py
+++ reader.py
@@ -1,20 +1,35 @@
 from SimpleCV import Color,Camera,Display
 import requests
 import RPi.GPIO as GPIO
+import time
 
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(27, GPIO.OUT)
 cam = Camera()
 display = Display()
 
+def blink(): 
+  GPIO.output(17, GPIO.HIGH)
+  time.sleep(1)
+  GPIO.output(17, GPIO.LOW) 
+
+def post_request(input_method, barcode):
+  
+
 while(display.isNotDone()):
 
+  input_method = 'add' if GPIO.input(17) else 'remove'
   img = cam.getImage()
   barcode = img.findBarcode()
+
   if(barcode is not None):
     barcode = barcode[0]
     result = str(barcode.data)
-    print 'test'
+    print 'Barcode found:'
     print result 
+    post_request(input_method, barcode)
+    blink()  
     barcode = []
   print 'None'
   img.save(display)
