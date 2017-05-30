10563557784276e06374b0f92b0113628c6cb959
John <johnwilles@gmail.com>
2017-05-30 12:49:41 -0400

    Add readme and circuit diagram

--- reader.py
+++ reader.py
@@ -3,17 +3,24 @@ import requests
 import RPi.GPIO as GPIO
 import time
 
+# GPIO init 
+method_pin = 17
+blink_pin = 27
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(27, GPIO.OUT)
+GPIO.setup(method_pin, GPIO.IN)
+GPIO.setup(blink_pin, GPIO.OUT)
+
+#SimpleCV init 
 cam = Camera()
 display = Display()
 
+# Blink LED 
 def blink(): 
-  GPIO.output(17, GPIO.HIGH)
+  GPIO.output(blink_pin, GPIO.HIGH)
   time.sleep(1)
-  GPIO.output(17, GPIO.LOW) 
+  GPIO.output(blink_pin, GPIO.LOW) 
 
+# Post to web app with upc
 def post_request(input_method, barcode):
   payload = { 'method': input_method, 'upc':  barcode }
   r = requests.post('http://localhost:3000/api', data=payload)
@@ -21,11 +28,15 @@ def post_request(input_method, barcode):
   
 
 while(display.isNotDone()):
-
-  input_method = 'add' if GPIO.input(17) else 'remove'
+  
+  # Check if method pin is high and set input method
+  input_method = 'add' if GPIO.input(method_pin) else 'remove'
+  
+  # Grab frame from cam and check for barcode 
   img = cam.getImage()
   barcode = img.findBarcode()
 
+  # Process barcode if found
   if(barcode is not None):
     barcode = barcode[0]
     result = str(barcode.data)
@@ -34,5 +45,7 @@ while(display.isNotDone()):
     post_request(input_method, barcode)
     blink()  
     barcode = []
-  print 'None'
+  else:
+    print 'None'
+  # Display frame
   img.save(display)
